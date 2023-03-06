964dd4e7537ce0e9ac46ce223cf246e9c8d65c84
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-03-06 16:03:02 +0100

    Lagt till delete. Save och load går att välja filväg

--- AddressList/Program.cs
+++ AddressList/Program.cs
@@ -7,6 +7,7 @@ namespace AddressList
 {
     internal class Program
     {
+        static string user = $"{Environment.GetEnvironmentVariable("USERPROFILE")}";
         static List<Person> AddressList = new List<Person>(20);
         class Person
         {
@@ -40,43 +41,24 @@ namespace AddressList
                     Write("Tryck enter för att ladda upp default-fil, annars skriv filens sökväg: ");
                     Load(ReadLine());
                 }
+                else if (command == "add")
+                {
+                    Add();
+                    WriteLine("Glöm inte att spara dina ändringar med \"save\".");
+                }
                 else if (command == "list")
                 {
                     foreach (Person p in AddressList)
                         Person.Print(p);
                 }
-                else if (command == "add")
-                {
-                    Write("Namn: ");
-                    string n = ReadLine();
-                    Write("Telefonnummer: ");
-                    string p = ReadLine();
-                    Write("Adress: ");
-                    string a = ReadLine();
-                    AddressList.Add(new Person(n, p, a));
-                    WriteLine("Glöm inte att spara dina ändringar med \"save\".");
-                }
-                //10
                 else if (command == "save")
                 {
-                    Write("Namnge filen att spara i, avsluta med \".txt\" : ");
-                    using (StreamWriter sw = new StreamWriter(ReadLine(), false))
-                    {
-                        foreach (Person p in AddressList)
-                        {
-                            string[] person = { p.name, p.phone, p.address };
-                            sw.WriteLine(String.Join(", ", person));
-                        }
-                    }
+                    
+                    Save();
                 }
-                //11
                 else if (command == "delete")
                 {
-                    Write("Vem ska raderas: ");
-                    string n = ReadLine();
-                    int i = AddressList.FindIndex(x => x.name.Contains(n));
-                    AddressList.RemoveAt(i);
-                    WriteLine("Glöm inte att spara dina ändringar med \"save\".");
+                    Delete();
                 }
                 else if (command == "quit")
                 {
@@ -91,25 +73,25 @@ namespace AddressList
         }
         public static void Help()
         {
-            WriteLine("load - lagrar adresser från valfri lokal fil, eller från defaultfil" +
-            "add - lagrar nya adresser i programmet" +
-            "list - skriver ut de lagrade adresserna i programmet" +
-            "delete - tar bort valfri adress ut programmet" +
-            "save - sparar lagrade adresser till default eller valfri fil" +
+            WriteLine("load - lagrar adresser från valfri lokal fil, eller från defaultfil\n" +
+            "add - lagrar nya adresser i programmet\n" +
+            "list - skriver ut de lagrade adresserna i programmet}\n" +
+            "save - sparar lagrade adresser till default eller valfri fil\n" +
+            "delete - tar bort valfri adress ut programmet\n" +
             "quit - avslutar utan att spara lagrade adresser");
         }
-        public static void Load(string path = null)
+        public static void Load(string path = "")
         {
             string filepath;
-            if (path == null)
-                filepath = $"{Environment.GetEnvironmentVariable("USERPROFILE")}\\Documents\\adresser.txt";
+            if (path == "")
+                filepath = $"{user}\\Documents\\adresser.txt";
             else
                 filepath = path;
             WriteLine("Laddar upp filen\n...");
             using (StreamReader sr = new StreamReader(filepath))
             {
-                string line = sr.ReadLine();
-                while (line != null)
+                string line;
+                while ((line = sr.ReadLine()) != null)
                 {
                     string[] parts = line.Split(',');
                     WriteLine($"Namn: {parts[0]}\nTelefon: {parts[1]}\nAdress: {parts[2]}\n");
@@ -117,6 +99,46 @@ namespace AddressList
                     AddressList.Add(p);
                 }
             }
+        }
+        public static void Add()
+        {
+            Write("Namn: ");
+            string n = ReadLine();
+            Write("Telefonnummer: ");
+            string p = ReadLine();
+            Write("Adress: ");
+            string a = ReadLine();
+            AddressList.Add(new Person(n, p, a));
+        }
+        public static void Save()
+        {
+            string filepath;
+            Write("Skriv Q för att avbryta.\nTryck enter för att spara i default-fil, annars skriv filnamn inklusive extension (.txt) : ");
+            string n = ReadLine();
+            if (n == "Q" || n == "q")
+                return;
+            if (n == null)
+                filepath = $"{user}\\Documents\\adresser.txt";
+            else
+                filepath = n;
+            using (StreamWriter sw = new StreamWriter(filepath, false))
+            {
+                foreach (Person p in AddressList)
+                {
+                    string[] person = { p.name, p.phone, p.address };
+                    sw.WriteLine(String.Join(", ", person));
+                }
+            }
+        }
+        static void Delete()
+        {
+            Write("Skriv Q för att avbryta.\nNamnge den som ska raderas: ");
+            string n = ReadLine();
+            if (n == "Q" || n == "q")
+                return;
+            int i = AddressList.FindIndex(x => x.name.Contains(n));
+            AddressList.RemoveAt(i);
+            WriteLine("Glöm inte att spara dina ändringar med \"save\".");
         }
     }
 }        
